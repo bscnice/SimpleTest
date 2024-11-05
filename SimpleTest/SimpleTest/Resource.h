b67bf777c265939869732c82a61360acb9076276
bscnice <bscnice@naver.com>
2024-11-05 18:44:23 +0900

    Add listbox and modify OnBnClickedMsgBtn function

--- SimpleTest/SimpleTest/Resource.h
+++ SimpleTest/SimpleTest/Resource.h
@@ -7,16 +7,16 @@
 #define IDS_ABOUTBOX                    101
 #define IDD_SIMPLETEST_DIALOG           102
 #define IDR_MAINFRAME                   128
-#define IDC_BUTTON2                     1001
 #define IDC_MSG_BTN                     1002
+#define IDC_LIST                        1003
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        129
+#define _APS_NEXT_RESOURCE_VALUE        130
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1003
+#define _APS_NEXT_CONTROL_VALUE         1004
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
