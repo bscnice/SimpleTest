f70bbf184b2269c52519b7f7b1e6e9482f0579bf
bscnice <bscnice@naver.com>
2024-11-05 19:39:32 +0900

    Add OnBnClickedResetBtn function

--- SimpleTest/SimpleTest/Resource.h
+++ SimpleTest/SimpleTest/Resource.h
@@ -9,6 +9,8 @@
 #define IDR_MAINFRAME                   128
 #define IDC_MSG_BTN                     1002
 #define IDC_LIST                        1003
+#define IDC_BUTTON3                     1004
+#define IDC_RESET_BTN                   1004
 
 // Next default values for new objects
 // 
@@ -16,7 +18,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        130
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1004
+#define _APS_NEXT_CONTROL_VALUE         1005
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
